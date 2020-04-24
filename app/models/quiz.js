2e055db7cfdbbaf860d0b72e7980cb0e0d8b7c89
torystosse <tory.mae.stosse@gmail.com>
2020-04-24 14:17:35 -0400

    Updated quiz model
    Added numOfQuestions to quiz model

--- app/models/quiz.js
+++ app/models/quiz.js
@@ -14,6 +14,10 @@ const quizSchema = new mongoose.Schema({
     type: mongoose.Schema.Types.ObjectId,
     ref: 'Classroom'
   }],
+  numOfQuestions: {
+    type: Number,
+    required: true
+  },
   isActive: {
     type: Boolean,
     required: true
