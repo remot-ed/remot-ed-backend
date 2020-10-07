5a61667cd1b624f077e2da6cf500e83b97c42dcc
Derek Murphy <derekmurphy1993@gmail.com>
2020-10-07 16:37:14 -0400

    Added firstName lastName to User Model
    Groundwork for future client side improvements

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,14 @@
 const mongoose = require('mongoose')
 
 const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: false
+  },
+  lastName: {
+    type: String,
+    required: false
+  },
   email: {
     type: String,
     required: true,
